61dfd2749aa66cb2df00589e9c09a28961b318b0
YoselynCastillo <yose.castillo06@gmail.com>
2023-01-31 13:37:39 -0400

    add videos

--- js/index.js
+++ js/index.js
@@ -1,34 +1,45 @@
-var titleasda = document.getElementById("title1")
-var titles = document.getElementsByClassName("fwdr3dcov-title")
+// var titles = document.getElementsByClassName("fwdr3dcov-title")
 
 var info = [
     {
         thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01070317_RND.png',
-        thumbVideoSrc: '',
+        thumbVideoSrc: 'TIAA_0-Forum2023-LOOP_DEL.mp4',
         title: 'Institutional Platforms Re-Imagined',
         desc: 'Reimagined Plan Sponsor and Consultant experiences to improve quality, reduce administrative time, and optimize their business',
     },
     {
-        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01070317_RND.png',
-        thumbVideoSrc: '',
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01043014_RND.png',
+        thumbVideoSrc: 'TIAA_1-PlanFocus_v09_DEL.mp4',
         title: 'Participant Experiences Re-Imagined',
         desc: 'Reimagined Web and Mobile experiences to provide delightful and transformation step change omni channel experiences that engage and support the participants through their life journey to achieve their retirement goals',
     },
     {
-        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01070317_RND.png',
-        thumbVideoSrc: '',
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01001213_RND.png',
+        thumbVideoSrc: 'TIAA_2-Web-Mobile_v07_DEL.mp4',
         title: 'Customer Servicing Re-Imagined',
         desc: 'Strategic Partnerships and AI driven platforms to provide the best in class customer servicing experience',
     },
     {
-        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01070317_RND.png',
-        thumbVideoSrc: '',
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_PLAYALL_RND.png',
+        thumbVideoSrc: 'TIAA_3-Gateway_v07_DEL.mp4',
         title: 'Retirement Gateway',
         desc: 'The next-generation engine that enables extended integration and external interconnectivity for a fully digital retirement offering ecosystem',
     },
     {
-        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01070317_RND.png',
-        thumbVideoSrc: '',
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01035317_RND.png',
+        thumbVideoSrc: 'TIAA_4-GoogleAI_v11_DEL.mp4',
+        title: 'Re-Imagined TIAA Retirement Technology',
+        desc: 'The next generation and transformative changes coming to our platforms and technology supporting Retirement',
+    },
+    {
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01074603_RND.png',
+        thumbVideoSrc: 'TIAA_5-MFABB_v11_DEL.mp4',
+        title: 'Re-Imagined TIAA Retirement Technology',
+        desc: 'The next generation and transformative changes coming to our platforms and technology supporting Retirement',
+    },
+    {
+        thumbSrc: 'TIAA_0-Forum2023-LOOP_DEL_01083623_RND.png',
+        thumbVideoSrc: 'TIAA_6-Awards_v01_DEL.mp4',
         title: 'Re-Imagined TIAA Retirement Technology',
         desc: 'The next generation and transformative changes coming to our platforms and technology supporting Retirement',
     },
@@ -51,7 +62,7 @@ for (let index = 1; index < info.length; index++) {
     console.log(info[index].thumbSrc)
 
     liAssets.setAttribute('data-thumb-src', `assets/${info[index].thumbSrc}`);
-    liAssets.setAttribute('data-thumb-video-src', `assets/${info[index].thumbVideoSrc}`);
+    liAssets.setAttribute('data-thumb-video-src', `assets/videos/${info[index].thumbVideoSrc}`);
     liAssets.setAttribute('data-thumb-vast-src', '');
 
     liText.setAttribute('data-thumb-caption', '');
